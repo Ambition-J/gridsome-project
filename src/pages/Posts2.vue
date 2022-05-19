5e200d0ac96fc6bdb68e4bbabc9b61c020f049b6
吴疆 <ajiang@bogon.host>
2022-05-19 23:28:55 +0800

    init

---
+++ src/pages/Posts2.vue
@@ -0,0 +1,46 @@
+<template>
+  <Layout>
+    <div>
+      <h1>Posts 1</h1>
+      <ul>
+        <li v-for="edge in $page.posts.edges" :key="edge.node.id">
+          <g-link :to="edge.node.path">{{ edge.node.title }}</g-link>
+        </li>
+      </ul>
+    </div>
+  </Layout>
+</template>
+<page-query>
+query {
+  posts:allPost {
+    edges {
+      node {
+        id
+        title
+        path
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Posts1',
+  data () {
+    return {
+      posts: []
+    }
+  },
+  async created () {
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    this.posts = data
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/templates/Post.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div>{{ $page.post.title }}</div>
+    <div>{{ $page.post.content }}</div>
+  </div>
+</template>
+<page-query>
+query ($id: ID!) {
+  post (id: $id) {
+    id
+    title
+    content
+  }
+}
+</page-query>
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: this.$page.post.title,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
